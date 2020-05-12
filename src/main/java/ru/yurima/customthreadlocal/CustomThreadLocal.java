ef6a341c4bdf7f6eeb9fb0057e427615cca38f1e
Yury <edenstack17@gmail.com>
2020-05-12 15:26:22 +0400

    add a comment

--- src/main/java/ru/yurima/customthreadlocal/CustomThreadLocal.java
+++ src/main/java/ru/yurima/customthreadlocal/CustomThreadLocal.java
@@ -4,6 +4,12 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+/**
+ * Я не являюсь крупным специалистом в concurrency. Но в чем проблема использовать такую простую реализацию?
+ * Заранее благодарен за разъяснения.
+ *
+ */
+
 public class CustomThreadLocal<T> implements IThreadLocal<T>{
     private final Map<Thread, T> store = Collections.synchronizedMap(new WeakHashMap<>());
 
